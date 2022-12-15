b1e4342ba0e5ea7887df323aa37b56a2c2ad0d75
remi.lopez <r.lopez76000@gmail.com>
2022-12-15 16:31:24 +0100

    test

--- controllers/Loaners_controllers.js
+++ controllers/Loaners_controllers.js
@@ -34,7 +34,7 @@ const material = await Materials.findById(loanedMaterial)
             var message = {
                 from: "testreminws@hotmail.com",
                 to: newLoaners.email,
-                subject: "Un emprunt a été réalisé",
+                subject: "Un emprunt a été réalisé mec",
                 text: "Bonjour , je vous informe qu'un emprunt a bien été réalisé au sein de la NWS uwu uwu",
                 html: "<p>Bonjour , je vous informe qu'un emprunt a bien été réalisé au sein de la NWS</p>",
             };
